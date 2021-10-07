aae0a3b90cc0a76e551b0974d5790ae2855813bf
AMOLKOLI1991 <amolkoli595@gmail.com>
2021-10-07 15:51:52 +0530

    GitHubFirstProject: This is Project Structure

---
+++ src/test/java/com/Amolit/GitHubFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Amolit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
